64ab70dc21020989190008b1c280cffb1fd46fac
Linuxscripts <linuxscripts@gmx.com>
2015-11-04 17:45:14 +0100

    Create LES_BusyBox_cmd_update.sh

---
+++ LES_BusyBox_cmd_update.sh
@@ -0,0 +1,18 @@
+##########################################################################
+#                         BusyBox_cmd_update                             #
+# made by Brian Peregrine, released under the GNU General Public License #
+##########################################################################
+
+#This script will
+#Remove tty
+#Remove ttysize
+#Remove getty
+#Remove less, (keeping only vi)
+#Remove rmdir (use rm -r command instead)
+#Remove rpm
+#Remove rpm2cpio
+#Add locate command
+#Add rename command
+#Add ssh
+
+#!/bin/sh
