387bc171fc18bd2f9fab9c49bc22393ebf445e5d
Linuxscripts <linuxscripts@gmx.com>
2015-11-26 10:31:27 +0100

    Update LES_BusyBox_cmd_update.sh

--- LES_BusyBox_cmd_update.sh
+++ LES_BusyBox_cmd_update.sh
@@ -16,3 +16,31 @@
 #Add ssh
 
 #!/bin/sh
+
+for all this, busybox will need to be rebuild appearantly, can't be done with
+deleting things from outside busybox, see 
+http://busybox.net/FAQ.html#backporting
+
+If you don't want to take the risk of replacing all applets by newer
+versions, you can employ this trick:
+
+Download most recent release, configure it with "make allnoconfig", then
+use "make menuconfig" to switch on just the applet you want to test and
+maybe a couple of tuning options. Then build Busybox.
+
+Then, on target system, delete the old applet symlink that points to
+your old Busybox, and replace it with the new Busybox binary, renamed to
+applet's name. In other words, if you want to replace only, say, httpd,
+then delete, say, /bin/httpd symlink (which points to your old Busybox),
+then run "cp /path/to/new/busybox /bin/httpd". (For some applets such as
+login, you'll also need to "chmod u+s" your new busybox binary.)
+
+Now you can test the new applet and post a more useful email to the
+mailing list, either "I see such and such bug even in latest release" or
+"I see such and such bug in release X.Y.Z, but it seems to be fixed in
+last release".
+
+Deleting the old symlink still leaves the old functionality in your
+existing old Busybox binary, you just wouldn't be using it anymore. If
+things will get even worse with new version, you can always restore the
+symlink.
