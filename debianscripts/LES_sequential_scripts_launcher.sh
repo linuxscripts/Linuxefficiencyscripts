d9790a7eceb22494212e19a15e31dc1116a6e85b
Linuxscripts <linuxscripts@gmx.com>
2015-12-09 13:27:49 +0100

    Update LES_sequential_scripts_launcher.sh

--- debianscripts/LES_sequential_scripts_launcher.sh
+++ debianscripts/LES_sequential_scripts_launcher.sh
@@ -15,10 +15,9 @@ touch LES_install_crunchbangOpenbox_andConky.sh LES_Linuxdirectorytree_changing.
 chmod+x LES_install_crunchbangOpenbox_andConky.sh LES_Linuxdirectorytree_changing.sh LES_slitaz_altering_programs.sh LES_internet_speed_increase_A LES_internet_speed_increase_B LES_internet_speed_increase_C LES_bootmenu_changing.sh LES_BusyBox_cmd_update.sh
 
 ./LES_install_crunchbangOpenbox_andConky.sh
-./LES_Linuxdirectorytree_changing.sh
-./LES_slitaz_altering_programs.sh
+./LES_Linuxmountpoint_changing.sh
+./LES_altering_programs.sh
 ./LES_internet_speed_increase_A
 ./LES_internet_speed_increase_B
 ./LES_internet_speed_increase_C
 ./LES_bootmenu_changing.sh
-./LES_BusyBox_cmd_update.sh
